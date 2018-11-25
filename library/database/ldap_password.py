81a83a401d60d5e9865cfad456d7f0055802e3df
Michael Tabolsky <mtabolsky@gmail.com>
2018-11-25 22:35:44 +0100

    Ouch

--- library/database/ldap_password.py
+++ library/database/ldap_password.py
@@ -100,7 +100,7 @@ def ldap_connection(uri, bind_dn=None, bind_password=None, timeout=10, validate_
             conn.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
             conn.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
         else:
-            module.log(msg='Disabling certificates validation')
+            module.log(msg='Not disabling certificates validation')
 
         if bind_dn and bind_password:
             conn.simple_bind_s(bind_dn, bind_password)
